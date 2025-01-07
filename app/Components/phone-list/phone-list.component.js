d5212acc7e1cc985b3589dcb2ccbabeedc1a75a8
Amir Mahboubi <amirmahboubi@outlook.com>
2025-01-07 16:49:39 +0330

    🏗️ change code structure and architectur step 4 - Directory and File Organization

--- app/Components/phone-list/phone-list.component.js
+++ app/Components/phone-list/phone-list.component.js
@@ -1,14 +1,9 @@
+"use strict";
+
 // Register `phoneList` component, along with its associated controller and template
-angular.module("tutorialAngularJsApp").component("phoneList", {
+angular.module("phoneList").component("phoneList", {
   // This name is what AngularJS uses to match to the `<phone-list>` element.
-  template:
-    "<ul>" +
-    '<li ng-repeat="phone in $ctrl.phones">' +
-    "<span>{{phone.name}}</span>" +
-    "<p>{{phone.snippet}}</p>" +
-    "</li>" +
-    "</ul>" +
-    "<p>Total number of phones: {{$ctrl.phones.length}}</p>",
+  templateUrl: "./phone-list.template.html",
   controller: function PhoneListController() {
     this.phones = [
       {
---
+++ app/app.module.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Define the `tutorialAngularJsApp` module
+angular.module('tutorialAngularJsApp', [
+  // ...which depends on the `phoneList` module
+  'phoneList'
+]);
