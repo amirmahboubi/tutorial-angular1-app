c76de0fa584b2622aa3d0b2b8879bed9e199d49a
Amir Mahboubi <amirmahboubi@outlook.com>
2025-01-15 09:32:57 +0330

    ✨ feat(phone-list.componenrs): use $http service and get data from phones.json

--- app/Components/phone-list/phone-list.component.js
+++ app/Components/phone-list/phone-list.component.js
@@ -4,29 +4,17 @@
 angular.module("phoneList").component("phoneList", {
   // This name is what AngularJS uses to match to the `<phone-list>` element.
   templateUrl: "Components/phone-list/phone-list.template.html",
-  controller: function PhoneListController() {
-    this.phones = [
-      {
-        name: "Nexus S",
-        snippet: "Fast just got faster with Nexus S.",
-        age: 1,
-      },
-      {
-        name: "Motorola XOOM™ with Wi-Fi",
-        snippet: "The Next, Next Generation tablet.",
-        age: 2,
-      },
-      {
-        name: "MOTOROLA XOOM™",
-        snippet: "The Next, Next Generation tablet.",
-        age: 3,
-      },
-      {
-        name: "Apple iphone 16™",
-        snippet: "Built for Apple Intelligence. Camera Control. 48MP Fusion camera. Five vibrant colors. A18 chip.",
-        age: 2,
-      },
-    ];
-    this.orderProp = "age";
-  },
+  controller: [
+    "$http",
+    function PhoneListController($http) {
+      var self = this;
+      $http.get("Data/phones/phones.json").then(function (response) {
+        self.phones = response.data;
+
+        //pre-process the HTTP response by limiting the number of phones to the first 5 in the list.
+        //self.phones = response.data.slice(0, 5);
+      });
+      self.orderProp = "age";
+    },
+  ],
 });
